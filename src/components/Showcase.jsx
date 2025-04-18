eb9afabe4a4450094c85ea45d2f41e293278bdb4
DvQb <davidresendiz753@gmail.com>
2025-04-18 08:47:23 -0600

    first commit

---
+++ src/components/Showcase.jsx
@@ -0,0 +1,17 @@
+import showcase from '../assets/img/showcase2.jpg'
+
+const Showcase = () => {
+
+
+    return (
+
+        <div className='w-full h-[3000px]'>
+
+         <img src={showcase} alt="" className='w-full'/>
+
+        </div>
+
+    )
+}
+
+export default Showcase;
